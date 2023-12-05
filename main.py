5a1b6d5db579c0d1ed2c055c1e39b1417b6efd7b
frla18cz <70630255+frla18cz@users.noreply.github.com>
2023-12-05 17:10:18 +0100

    Update main.py

--- main.py
+++ main.py
@@ -44,16 +44,13 @@ def send_initial_message():
     st.session_state.messages = [{"role": "assistant", "content": initial_message}]
     send_message_to_openai(initial_message)
 
-def reset_messages():
-    if st.button("Reset Messages"):
-        st.session_state.messages = []
 
 def exit_chat():
     """Ukončí chatovací session a vymaže historii chatu."""
     if st.button("Exit Chat"):
         st.session_state.messages = []  # Clear the chat history
         st.session_state.thread_id = None
-        reset_messages()
+        st.experimental_rerun()  # Reset the chat session
     process_user_input()
     lottie_animation("https://lottie.host/2b556f4b-1b93-477e-a421-9e31f4511246/tKYol4Wo3r.json",3) 
 
