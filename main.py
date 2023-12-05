2df3b15f559edb20c4de953d8dac88aaf90960e6
frla18cz <70630255+frla18cz@users.noreply.github.com>
2023-12-05 22:07:35 +0100

    Update main.py

--- main.py
+++ main.py
@@ -61,7 +61,7 @@ def display_messages():
 
 def process_user_input():
     """Zpracovává uživatelský vstup a odesílá jej do OpenAI."""
-    prompt = st.text_input("...")
+    prompt = st.chat_input("...")
     if prompt:
         st.write("Já :-):", prompt)
 
